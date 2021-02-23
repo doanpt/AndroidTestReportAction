475a79770669e41f625124373b024596f5b14c83
Trung Doan <doanpt93cntt@gmail.com>
2021-02-23 11:59:43 +0700

    display detail failed tese

--- extractReport.py
+++ extractReport.py
@@ -1,9 +1,11 @@
 import sys
 import re
+import os
 from bs4 import BeautifulSoup
 def parseXML(file):
     f = open(file, encoding="utf8")     
     soup = BeautifulSoup(f, "html.parser")
+
     #parse sumary
     sumaryContent = soup.find_all("div", {"class":"infoBox"})
     if len(sumaryContent)>0:
@@ -19,7 +21,6 @@ def parseXML(file):
     #parse detail
     regex = re.compile('tab *')
     contentDiv = soup.find_all("div", {"class":regex})
-    
     for tIndex in range(len(contentDiv), 1 if len(contentDiv) > 2 else 0, -1):
         print("-----------------------------------------------------------------------------------------------------------")
         titles = []
@@ -46,6 +47,45 @@ def parseXML(file):
         print(getTitleFormat(titles, max))
         print(getContentFormat(titles, contents, max))
     print("-----------------------------------------------------------------------------------------------------------")
+    #print detail message
+    if len(contentDiv) > 2:
+        print("Failed tests:\n")
+        failLink = contentDiv[0].find_all("a")
+        for i in range(1,len(failLink),2):
+            print("----------------------------------------------")
+            print(printFormatter("Name:", failLink[i].text))
+            fileName = failLink[i-1]['href']
+            if failLink[i-1]['href'].startswith("classes/"):
+                fileName = failLink[i-1]['href'][failLink[i-1]['href'].rindex("/")+1:]
+            print(printFormatter("File:", fileName))
+            detailFile = findFileByNameInFolder(file[:file.rindex("/")], fileName)
+            extractDetailFailed(detailFile, failLink[i].text)
+            
+def extractDetailFailed(file, testName):
+    f = open(file, encoding="utf8")     
+    soup = BeautifulSoup(f, "html.parser")
+    faileTables = soup.find_all("table")
+    faildTds = faileTables[2].find_all("td")
+    failedNameTags = soup.find_all("h3",{"class":"failures"})
+    messages = soup.find_all("span",{"class":"code"})
+    for i in range(len(faildTds)):
+        if(faildTds[i].text == testName):
+            print(printFormatter("Duration:", faildTds[i+1].text))
+            messageIndex = findMessageIndex(testName, failedNameTags)
+            print(printFormatter("Message:", messages[messageIndex].text.replace("\n","",1)))
+
+def findMessageIndex(testName, failedNameTags):
+    for i in range(len(failedNameTags)):
+        if failedNameTags[i].text == testName:
+            return i
+    return -1
+
+def findFileByNameInFolder(sFold, sName):
+    for root, dirs, files in os.walk(sFold):
+        for name in files:
+            if name == sName:
+                return os.path.join(root, name)
+    return null
 
 def getContentFormat(title, content, max):
     t = ""
